e821fb47bbfab8926cf95be42b4debf50a1e2c85
Simone <smgabionetta@yahoo.com.br>
2025-10-02 15:15:47 +0100

    adcionando breforeEach do login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,29 +1,28 @@
 describe('Login', () => {
-  it('Login com dados válidos deve permitir entrada no sistema', () => {
-    //Arrange
+
+  beforeEach(() => {
+    // Arrange
     cy.visit('http://localhost:4000')
-
-    //Act
-    cy.get('#username').click().type('julio.lima')
-    cy.get('#senha').click().type('123456')
-    //cy.get('#login-section > .btn').click()
-    cy.contains('button','Entrar').click()
-
-    //Assert
-    cy.contains('h4','Realizar Transferência').should('be.visible')
-
   })
 
-  it ('Login com dados inválidos deve apresentar mensagem de erro', () => {
-    //Arrange
-    cy.visit('http://localhost:4000')
+  it('Login com dados válidos deve permitir entrada no sistema', () => {
+    // Act
+    cy.get('#username').click().type('julio.lima')
+    cy.get('#senha').click().type('123456')
+    // cy.get('#login-section > .btn').click()
+    cy.contains('button','Entrar').click()
 
-    //Act
+    // Assert
+    cy.contains('h4','Realizar Transferência').should('be.visible')
+  })
+
+  it('Login com dados inválidos deve apresentar mensagem de erro', () => {
+    // Act
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('654321')
     cy.contains('button','Entrar').click()
 
-    //Assert
+    // Assert
     cy.get('.toast').should('have.text', 'Erro no login. Tente novamente.')
   })
 

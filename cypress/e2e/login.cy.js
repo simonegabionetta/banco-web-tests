75621804be2914dcb1cea28159e2a102df16a0f1
Simone <smgabionetta@yahoo.com.br>
2025-10-16 20:10:04 +0100

    Implementando comandos customizados para login, transferências e Commons

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,32 +1,21 @@
 describe('Login', () => {
-
   beforeEach(() => {
     // Arrange
-    cy.visit('/')    
+    cy.visit('/')       
   })
 
   it('Login com dados válidos deve permitir entrada no sistema', () => {
-    // Act
-    cy.fixture('credenciais').then(credenciais => {
-      cy.get('#username').click().type(credenciais.valida.usuario)
-      cy.get('#senha').click().type(credenciais.valida.senha)   
-    })     
-    
-    cy.contains('button','Entrar').click()    
-
-    // Assert
+    //Act
+    cy.fazerLoginComCredenciaisValidas()  
+   //Assert
     cy.contains('h4','Realizar Transferência').should('be.visible')
   })
 
   it('Login com dados inválidos deve apresentar mensagem de erro', () => {
-    // Act
-    cy.fixture('credenciais').then(credenciais => {
-      cy.get('#username').click().type(credenciais.invalida.usuario)
-      cy.get('#senha').click().type(credenciais.invalida.senha)   
-    }) 
-    cy.contains('button','Entrar').click()
-
-    // Assert
-    cy.get('.toast').should('contain.text', 'Erro no login. Tente novamente.')
+    //Act
+     cy.fazerLoginComCredenciaisInvalidas()  
+    //Assert
+    cy.verificarMensagemNoToast('Erro no login. Tente novamente.')
   })
+
 })
